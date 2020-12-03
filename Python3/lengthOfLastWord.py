72599b8664d7450e9275520c45ce594fdb11e7ac
gopalk <kaylingopal@gmail.com>
2020-12-03 18:51:49 +0200

    leets

--- Python3/lengthOfLastWord.py
+++ Python3/lengthOfLastWord.py
@@ -6,8 +6,15 @@ A word is a maximal substring consisting of non-space characters only.
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        words = s.split(' ')
-        return len(words[-1])
-    
-
+        #Clean up string 
+        s_clean = s.strip()
+        length:int = 0
         
+        for i in range(1,(len(s_clean)+1)):
+            if s_clean[-i] == ' ':
+                break
+            else:
+                length += 1
+                continue
+        
+        return length 
