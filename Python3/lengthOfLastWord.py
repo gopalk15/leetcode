be6d1ce4bf83f9498b060df552e1ad661bbca64d
gopalk <kaylingopal@gmail.com>
2020-12-03 10:37:49 +0200

    Day 1 Solution

---
+++ Python3/lengthOfLastWord.py
@@ -0,0 +1,13 @@
+'''
+Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+
+A word is a maximal substring consisting of non-space characters only.
+'''
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split(' ')
+        return len(words[-1])
+    
+
+        
